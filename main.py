dd88fab12a364156be0d803a70b90d5a97ef7a70
Drake <dferber@sjnma.org>
2017-04-06 09:15:45 -0500

    finally finished

--- main.py
+++ main.py
@@ -9,44 +9,63 @@ player2roll2=randrange(1,6)
 player2roll3=randrange(1,12)
 player2roll4=randrange(1,20)
 
-wincount=3
-player1score=player1roll1 + player1roll2 + player1roll3 + player1roll4
-player2score=player2roll1 + player2roll2 + player2roll3 + player2roll4 
-
+wincount=3 
+player1score=0
+player2score=0
 
 
 while(player1score != 3 and player2score != 3):
     player1roll1 = randrange(1,4)
-    player1roll2 = randrange(1,6)
-    player1roll3 = randrange(1,12)
-    player1roll4 = randrange(1,20)
     player2roll1 = randrange(1,4)
-    player2roll2 = randrange(1,6)
-    player2roll3 = randrange(1,12)
-    player2roll4 = randrange(1,20)
-    
+    print(player2roll1,player1roll1)
+    if(player1roll1 == 1 and player2roll1 == 1):
+        print("TIE")
+    if(player1roll1 == 1):
+        print("PLAYER 1 WINS")
+        player1score=3
     if(player1roll1 > player2roll1):
-        player1score = player1score + 1
+            print("player 1")
+            player1score =+ 1
     if(player2roll1 > player1roll1):
-	    player2score = player2score + 1
+            print("player 2")
+            player2score =+ 1
 
+
+    player1roll2 = randrange(1,6)
+    player2roll2 = randrange(1,6)
+    print(player1roll2,player2roll2)
     if(player1roll2 > player2roll2):
-    	player1score = player1score + 1
+        print("player 1")
+        player1score =+ 1
     if(player2roll2 > player2roll2):
-    	player2score = player2core + 1
+        print("player 2")
+        player2score =+ 1
 
+    player1roll3 = randrange(1,12)
+    player2roll3 = randrange(1,12)
+    print(player1roll3,player2roll3)
+    if(player1roll3 == 3):
+        print("PLAYER 2 WINS")
+    if(player1roll3 == 3 and player2roll3 == 3):
+        print("TIE")
     if(player1roll3 > player2roll3):
-    	player1score = player1score + 1
+        print("player 1")
+        player1score =+ 1
     if(player2roll3 > player1roll3):
-        player2score = player2score + 1
+        print("player 2")
+        player2score =+ 1
 
+    player1roll4 = randrange(1,20)
+    player2roll4 = randrange(1,20)
+    print(player1roll4,player2roll4)
     if(player1roll4 > player2roll4):
-        player1score = player1score + 1
+        print("player 1")
+        player1score =+ 1
     if(player2roll4 > player1roll4):
-        player2score = player2score + 1
-
-        
-    if(player1score == 3):
-    	print("PLAYER 1 WINS!")
-    if(player2score == 3):
-    	print("PLAYER 2 WINS!")
+        print("player 2")
+        player2score =+ 1
+   
+if(player1score == 3):
+    print("PLAYER 1 WINS!")
+if(player2score == 3):
+    print("PLAYER 2 WINS!")
